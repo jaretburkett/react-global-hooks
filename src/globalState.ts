869810a6df7f732e1c1a986e48a209e3615390ba
Jaret Burkett <jaretburkett@gmail.com>
2021-02-04 11:08:56 -0600

    Fixed memory leak of stacking callbacks

--- src/globalState.ts
+++ src/globalState.ts
@@ -7,6 +7,7 @@ export class GlobalState<T> {
   }
   private _value: T;
   private _hooks: ((newVal: T) => void)[] = [];
+  private _setValues:any[] = [];
 
   _addHook = (hook: (newVal: T) => void) => {
     this._hooks.push(hook);
@@ -29,7 +30,8 @@ export class GlobalState<T> {
   set = (newVal: T) => {
     this._value = newVal;
     // keep an array of hooks to remove that no longer exist
-    let toRemove = [];
+    let toRemoveHooks = [];
+    let toRemoveSetValues = [];
     // trigger the state update for each registered hook
     for (let i = 0; i < this._hooks.length; i++) {
       if(this._hooks[i]){
@@ -37,16 +39,35 @@ export class GlobalState<T> {
           this._hooks[i](newVal)
         } catch (e) {
           // threw an error, component muse have unmounted, add to remove list
-          toRemove.push(this._hooks[i]);
+          toRemoveHooks.push(this._hooks[i]);
         }
       } else {
         // hook no longer exist, add to remove list
-        toRemove.push(this._hooks[i]);
+        toRemoveHooks.push(this._hooks[i]);
+      }
+    }
+    // trigger set values
+    for (let i = 0; i < this._setValues.length; i++) {
+      if(this._setValues[i]){
+        try {
+          this._setValues[i](newVal)
+        } catch (e) {
+          // threw an error, component muse have unmounted, add to remove list
+          toRemoveSetValues.push(this._setValues[i]);
+        }
+      } else {
+        // hook no longer exist, add to remove list
+        toRemoveSetValues.push(this._setValues[i]);
       }
     }
     // remove any that do not exist or failed
-    for(let i = 0; i < toRemove.length; i++){
-      this._hooks.splice(this._hooks.indexOf(toRemove[i]), 1)
+    for(let i = 0; i < toRemoveHooks.length; i++){
+      this._hooks.splice(this._hooks.indexOf(toRemoveHooks[i]), 1)
+    }
+
+    // remove any that do not exist or failed
+    for(let i = 0; i < toRemoveSetValues.length; i++){
+      this._setValues.splice(this._setValues.indexOf(toRemoveSetValues[i]), 1)
     }
   }
 
@@ -59,8 +80,16 @@ export class GlobalState<T> {
   }
 
   onChange = (callback:(newVal: T) => void) => {
-    this._addHook(callback);
+    if(!(this._hooks.includes(callback))){
+      this._addHook(callback);
+    }
   } 
+  
+  _addSetValueHook = (setValue:any) => {
+    if(!(this._setValues.includes(setValue))){
+      this._setValues.push(setValue);
+    }
+  }
 }
 
 export const createGlobalState = <S>(initialValue: S): GlobalState<S> => {
@@ -72,9 +101,10 @@ export const useGlobalState = <S>(state: GlobalState<S>): [S, (newState: S) => v
   // piggyback on the react hook useState
   const [value, setValue] = useState(state.get());
   // add use state set hook to the state hooks
-  state._addHook((newVal:S) => {
-    setValue(newVal);
-  });
+  // state._addHook((newVal:S) => {
+  //   setValue(newVal);
+  // });
+  state._addSetValueHook(setValue)
   // return in a same use format as react useState hook
   return [value, state.set]
 }
